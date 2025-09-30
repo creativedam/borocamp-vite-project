e2f7c1b459943161af2cf6ad6ecd7006aab8cbbc
Gomolemo Ditlhong <gomolemoditlhong@gmail.com>
2025-09-30 22:27:44 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,28 @@
 
 </script>
 
-<template class="bg-white w-full">
+<template>
 
 
-<div class="bg-white w-full container ">
-      <div >
-      <a href="https://vite.dev" target="_blank">
+  <div class="bg-white w-full">
+    <div>
+      <a href="/" target="_blank">
         <img src="./assets/logo.png" class="logo" alt="Borocamp logo" />
       </a>
     </div>
     <div class="flex flex-row p-4">
-      <div class="basis-64 px-4"> 
-        <router-link to="/"
-          class=""><span class=" text-amber-600 text-3xl hover:text-amber-800 font-semibold">Home</span></router-link>
-        </div>
       <div class="basis-64 px-4">
-        <router-link to="/about"
-          class=""><span class=" text-amber-600 text-3xl hover:text-amber-800 font-semibold">About</span>
+        <router-link to="/" class="">
+          <span class=" text-amber-600 text-3xl hover:text-amber-800 font-semibold">Home</span>
         </router-link>
-        </div>
+      </div>
+      <div class="basis-64 px-4">
+        <router-link to="/about" class="">
+          <span class=" text-amber-600 text-3xl hover:text-amber-800 font-semibold">About</span>
+        </router-link>
+      </div>
 
     </div>
     <router-view />
-</div>
+  </div>
 </template>
