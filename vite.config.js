34d773392b0312436d0d18e996e85fd7edf891e9
Gomolemo Ditlhong <gomolemoditlhong@gmail.com>
2025-09-30 21:55:40 +0200

    commit gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+     tailwindcss(),
+  ],
+  base:"/borocamp-vite-project",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+},
 })
