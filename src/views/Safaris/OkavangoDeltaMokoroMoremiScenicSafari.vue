c32c0bcaf87eab5a5d709bcff7d060a58dc82762
Gomolemo Ditlhong <gomolemoditlhong@gmail.com>
2025-12-09 14:55:49 +0200

    commit

--- src/views/Safaris/OkavangoDeltaMokoroMoremiScenicSafari.vue
+++ src/views/Safaris/OkavangoDeltaMokoroMoremiScenicSafari.vue
@@ -13,23 +13,23 @@
         <SafariItinerary title="SAFARI DETAILS" :days="[
             {
                 title: 'Day 1',
-                description: 'You will be welcomed by one of our staff...'
+                description: 'You will be welcomed by one of our staff on your arrival and then you will be transferred to our camp in Boro. The camp is about 20 km from Maun Airport. At camp, you will find awaiting an oasis of rest among the tall and unique riverine vegetation. Our camp is located on the banks of the Boro River with expansive views of the horizon and its breath-taking sunsets. You will be welcomed to camp by a traditional performance in dance and song before settling to dinner.'
             },
             {
                 title: 'Day 2',
-                description: 'Following an early breakfast at Boro camp...'
+                description: 'Following an early breakfast at Boro camp, you will head to the Mokoro boat station. You will be transferred to the Mokoro (dugout canoe) and a Poler /professional guide will take over from the local guide. You will course through the intertwining streams and the riverine vegetation encountering a variety of birds and animals. Following a light lunch at your next camp, you will embark on a walking safari after an afternoon siesta. After a wholesome dinner enjoy stories and folklore tales around the crackling fire and the clear skies.'
             },
             {
                 title: 'Day 3',
-                description: 'Enjoy the magical early morning walking safari...'
+                description: 'Enjoy the magical early morning walking safari followed by lunch. Thereafter relax a little before returning to Boro Camp where a wholesome dinner will be served.'
             },
             {
                 title: 'Day 4',
-                description: 'An early start to Moremi Game Reserve...'
+                description: ' An early start to Moremi game reserve via the Moremi south gate. Spend the day exploring and tracking the animals. After a light packed lunch at a suitable rest place continue with the game drive before returning to camp in the late afternoon. Enjoy a wholesome dinner and enjoy folklore tales and stories of the day around the Boma.'
             },
             {
                 title: 'Day 5',
-                description: 'Wake up to breakfast. Transfer to the airport...'
+                description: 'Wake up to breakfast. Your transfer to the airport will be ready to take you at check out.'
             }
         ]" />
 
